96eada17c188f8641aec7dd4ffc70cc959a5b5e5
Soichiro-Yoshimura <yoshimura@soichiro.org>
2014-03-20 19:07:46 +0900

    add OOP samples

---
+++ src/NotOOPExsample.scala
@@ -0,0 +1,26 @@
+import scala.collection.mutable
+
+object NotOOPExample {
+
+  def main(args: Array[String]) {
+    val limit = args(0).toInt
+    for (index <- 1 to limit) {
+      index match {
+        case i if (i % 3 == 0 && i % 5 == 0) => printlnWithCount("Fizz Buzz")
+        case i if (i % 3 == 0) => printlnWithCount("Fizz")
+        case i if (i % 5 == 0) => printlnWithCount("Buzz")
+        case i => println(i)
+      }
+    }
+    countMap.foreach {
+      case (key, value) => println(s"$key : $value")
+    }
+  }
+
+  var countMap = mutable.Map[String, Int]()
+
+  def printlnWithCount(massage: String) = {
+    println(massage)
+    countMap.put(massage, countMap.getOrElse(massage, 0) + 1)
+  }
+}
---
+++ src/OOPExample.scala
@@ -0,0 +1,44 @@
+object OOPExample {
+  def main(args: Array[String]) {
+    val limit = args(0).toInt
+    for (index <- 1 to limit) {
+      PrintCountingFizzBuzzFinder.find(index).printMessage
+    }
+    PrintCountingFizzBuzzFinder.fizz.printCount
+    PrintCountingFizzBuzzFinder.buzz.printCount
+    PrintCountingFizzBuzzFinder.fizzBuzz.printCount
+  }
+}
+
+object PrintCountingFizzBuzzFinder {
+  val fizz = new Fizz
+  val buzz = new Buzz
+  val fizzBuzz = new FizzBuzz
+
+  def find(order: Int): FizzBuzzTerm = {
+    order match {
+      case o if (o % 3 == 0 && o % 5 == 0) => fizzBuzz
+      case o if (o % 3 == 0) => fizz
+      case o if (o % 5 == 0) => buzz
+      case o => new Number(o)
+    }
+  }
+}
+
+trait FizzBuzzTerm {
+  var countOfPrintMessage = 0
+  def message: String
+  def printMessage = {
+    println(message)
+    countOfPrintMessage += 1
+  }
+  def printCount = println(s"$message : $countOfPrintMessage")
+}
+
+class Fizz() extends FizzBuzzTerm { def message = "Fizz"}
+
+class Buzz() extends FizzBuzzTerm { def message = "Buzz"}
+
+class FizzBuzz() extends FizzBuzzTerm { def message = "Fizz Buzz"}
+
+class Number(val number: Int) extends FizzBuzzTerm { def message = number.toString }
